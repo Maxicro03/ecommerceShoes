614da9ebb4c0bd2efa04094ec5f56c9d84cb58a7
Maxicro <maximo.crocce03@gmail.com>
2023-08-28 02:01:57 -0300

    correcion localizacion banner

--- src/componnents/banner/banner.jsx
+++ src/componnents/banner/banner.jsx
@@ -1,7 +1,7 @@
 const Banner = () => {
     return(
         <div className="contenedorBanner">
-            <img src="./src/assets/componnents/images/banner.jpg" className="bannerPrincipal" alt="banner" />
+            <img src="./src/componnents/images/banner.jpg" className="bannerPrincipal" alt="banner" />
         </div>
     )
 }
